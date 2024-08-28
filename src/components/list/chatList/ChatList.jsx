d20b25114fbdfe25e74fefcc20804385daeb7ad8
Marco Lee <marcole32k3@gmail.com>
2024-08-27 22:52:52 -0700

    files

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react";
+import "./chatList.css";
+import AddUser from "./addUser/addUser";
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+import { useChatStore } from "../../../lib/chatStore";
+
+const ChatList = () => {
+  const [chats, setChats] = useState([]);
+  const [addMode, setAddMode] = useState(false);
+  const [input, setInput] = useState("");
+
+  const { currentUser } = useUserStore();
+  const { chatId, changeChat } = useChatStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userchats", currentUser.id),
+      async (res) => {
+        const items = res.data().chats;
+
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          const user = userDocSnap.data();
+
+          return { ...item, user };
+        });
+
+        const chatData = await Promise.all(promises);
+
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
+
+  const handleSelect = async (chat) => {
+    const chatIndex = chats.findIndex((item) => item.chatId === chat.chatId);
+  
+    // If chat is found, update only the `isSeen` field for that chat
+    if (chatIndex !== -1) {
+      const chatPath = `chats.${chatIndex}.isSeen`;
+  
+      const userChatsRef = doc(db, "userchats", currentUser.id);
+  
+      try {
+        await updateDoc(userChatsRef, {
+          [chatPath]: true,
+        });
+        changeChat(chat.chatId, chat.user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const filteredChats = chats.filter((c) =>
+    c.user.username.toLowerCase().includes(input.toLowerCase())
+  );
+
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      {filteredChats.map((chat) => (
+        <div
+          className="item"
+          key={chat.chatId}
+          onClick={() => handleSelect(chat)}
+          style={{
+            backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
+          }}
+        >
+          <img
+            src={
+              chat.user.blocked.includes(currentUser.id)
+                ? "./avatar.png"
+                : chat.user.avatar || "./avatar.png"
+            }
+            alt=""
+          />
+          <div className="texts">
+            <span>
+              {chat.user.blocked.includes(currentUser.id)
+                ? "User"
+                : chat.user.username}
+            </span>
+            <p>{chat.lastMessage}</p>
+          </div>
+        </div>
+      ))}
+
+      {addMode && <AddUser />}
+    </div>
+  );
+};
+
+export default ChatList;
